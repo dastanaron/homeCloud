2c576569881925d980afe45a701b006029896d06
dastanaron <flow199@yandex.ru>
2019-12-11 22:01:10 +0300

    Installed Nuxt for frontend interface

---
+++ frontend/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        {{ $t('home.title') }}
+      </div>
+      <v-card></v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+
+  }
+}
+</script>
